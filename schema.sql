bf35109d30673f659113a9319df45add55a21e90
Ross F <rfahan@gmail.com>
2023-08-02 14:22:40 -0600

    set up database schema/seed

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movie_names(
+    id INT IF NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    names VARCHAR(100)
+);
+
+CREATE TABLE reviews(
+    id INT IF NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    movie_key INT,
+    reviews TEXT,
+    FOREIGN KEY (movie_key)
+    REFERENCES movie_names(id)
+    ON DELETE SET NULL
+);
---
+++ seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO movie_names (names)
+VALUES ("Intro to JavaScript"),
+       ("Data Science"),
+       ("Linear Algebra"),
+       ("History of the Internet"),
+       ("Machine Learning"),
+       ("Game Design"),
+       ("Cloud Development");
+
+
+       INSERT INTO movie_names (names)
+VALUES ("MOVIE REVIEW 1"),
+       ("MOVIE REVIEW 2"),
+       ("MOVIE REVIEW 3"),
+       ("MOVIE REVIEW 4");
+       
+       
